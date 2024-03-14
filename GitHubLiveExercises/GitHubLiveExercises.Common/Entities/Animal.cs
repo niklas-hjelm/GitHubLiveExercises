eab1d82efe7ebb9b14ec237ad227e35f96ac5288
Heimbrand <olle.heimbrand@hotmail.se>
2024-03-14 10:50:36 +0100

    Added fk, forgot and only added icollections at first.

--- GitHubLiveExercises/GitHubLiveExercises.Common/Entities/Animal.cs
+++ GitHubLiveExercises/GitHubLiveExercises.Common/Entities/Animal.cs
@@ -1,8 +1,11 @@
-﻿namespace GitHubLiveExercises.Common.Interfaces;
+﻿using System.Security.AccessControl;
+
+namespace GitHubLiveExercises.Common.Interfaces;
 
 public class Animal
 {
     public int Id { get; set; }
+    public int PersonId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
     public ICollection<Person> Owners { get; set; }
--- GitHubLiveExercises/GitHubLiveExercises.Common/Entities/Person.cs
+++ GitHubLiveExercises/GitHubLiveExercises.Common/Entities/Person.cs
@@ -3,6 +3,7 @@
 public class Person
 {
     public int  Id { get; set; }
+    public int AnimalId { get; set; }
     public string Name { get; set; }
     public ICollection<Animal> Pets { get; set; }
 }
