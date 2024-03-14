9ea5ab0873509a6e4f482b4cb4caa0b6ba4d7f75
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2024-03-14 10:50:54 +0100

    Created generic IRepository interface

---
+++ GitHubLiveExercises/GitHubLiveExercises.Common/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace GitHubLiveExercises.Common.Interfaces;
+
+public interface IRepository<TEntity, TId>
+{
+    public Task<IEnumerable<TEntity>> GetAllAsync();
+
+    public Task<TEntity> GetByIdAsync(TId id);
+
+    public Task AddAsync(TEntity entity);
+
+    public Task UpdateAsync(TEntity entity);
+    public Task DeleteAsync(TEntity entity);
+
+}
